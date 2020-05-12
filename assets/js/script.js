c7613b9124045f111d2f5fc8c7d3dc1511076c23
Mariana <mariana_oliveira_9@hotmail.com>
2020-05-12 19:58:56 +0100

    js updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,34 @@
-const NAV = document.querySelector(".nav")
-var x = window.matchMedia("(max-width: 1000px)")
+const NAV = document.querySelector(".nav");
+var x = window.matchMedia("(max-width: 1000px)");
 
 
 //Change the carousel pictures number
 function changeSlider(x){
-    if(x.matches) {//If media query matches
-        new Glide('.glide', {
-        type: 'carousel',
-        startAt: 0,
-        perView: 1,
-        focusAt: 'center',
-        }).mount()
-    } else{
-        new Glide('.glide', {
-        type: 'carousel',
-        startAt: 0,
-        perView: 3,
-        focusAt: 'center',
-        }).mount()
-    }
+	if(x.matches) {//If media query matches
+		new Glide(".glide", {
+			type: "carousel",
+			startAt: 0,
+			perView: 1,
+			focusAt: "center",
+		}).mount();
+	} else{
+		new Glide(".glide", {
+			type: "carousel",
+			startAt: 0,
+			perView: 3,
+			focusAt: "center",
+		}).mount();
+	}
 }
 
-changeSlider(x) //Call listener function ate runtime
-x.addListener(changeSlider) //Attach listener function on state changes
+changeSlider(x); //Call listener function ate runtime
+x.addListener(changeSlider); //Attach listener function on state changes
 
 
 
 // Toggle Header Menu
 function toggleMenu(e){
-    NAV.classList.toggle('hide-mobile')
-       e.preventDefault();
+	NAV.classList.toggle("hide-mobile");
+	e.preventDefault();
 }
 
-// Exit Header Menu
-function exitMenu(e){
-    
-}
